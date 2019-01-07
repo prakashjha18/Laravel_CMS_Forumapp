fcfcbde97cd46fdb42ccea397f93d62eca7b229c
Unknown <prkjha25@gmail.com>
2019-01-07 19:06:37 +0530

    styling

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -4,10 +4,22 @@
     @foreach($discussions as $d)
         <div class="panel panel-default">
             <div class="panel-heading">
-                <img src="{{asset($d->user->avatar)}}" alt="{{asset($d->user->avatar)}}" width="70px" height="70px">
+                <img src="{{asset($d->user->avatar)}}" alt="{{asset($d->user->avatar)}}" width="40px" height="40px">&nbsp;&nbsp;&nbsp;
+                <span>{{$d->user->name}}</span>
+                <a href="{{route('discussion',['slug' => $d->slug])}}" class="btn btn-default pull-right">View</a>
             </div>
             <div class="panel-body">
-                {{$d->content}}
+                <h5 class="text-center">
+                    {{$d->title}}
+                </h5>
+                <p class="text-center">
+                    {{str_limit($d->content,50)}}
+                </p>
+            </div>
+            <div class="panel-footer">
+                <p>
+                    {{$d->replies->count()}}
+                </p>
             </div>
         </div>
     @endforeach
